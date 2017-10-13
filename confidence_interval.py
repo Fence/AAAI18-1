29c46702e8af3f44879037708cedae709adac69a
wuga214 <wuga214@gmail.com>
2017-10-13 17:42:12 -0400

    Ready to do time evaluation

--- confidence_interval.py
+++ confidence_interval.py
@@ -1,30 +1,38 @@
 import os
 import gc
 import json
+import numpy as np
 import tensorflow as tf
 #os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 from experiments.configuration import CONFIGURATIONS
 
-Data = {}
-for config in CONFIGURATIONS:
+
+def multiple_run(config, num_run=10):
     for index, step in enumerate(config['step']):
-        gc.collect()
-        tf.reset_default_graph()
-        with tf.Session() as sess:
-            name = (config['optimizer'].__name__
-                    + ' ' + config['domain'].__name__
-                    + ' ' + config['instance'][index]['name']
-                    + ' Planning Step:' + str(step))
-            print name
-            initial_a = tf.truncated_normal(shape=[config['batch'], step, config['dimension']],
-                                            mean=config['initial_mean'],
-                                            stddev=config['initial_std'])
-            a = tf.Variable(initial_a, name="action")
-            rnn_inst = config['optimizer'](a, step, config['batch'], config['domain'], config['instance'][index], sess=sess)
-            mean, std = rnn_inst.Optimize(config['epoch'])
-            print 'mean: {0}, std: {1}'.format(mean, std)
-            Data[name] = unicode([mean, std])
-with open('result.json', 'w') as fp:
-    json.dump(Data, fp)
+        name = (config['optimizer'].__name__
+                + ' ' + config['domain'].__name__
+                + ' ' + config['instance'][index]['name']
+                + ' Planning Step:' + str(step))
+        print name
+        records = []
+        for i in xrange(num_run):
+            gc.collect()
+            tf.reset_default_graph()
+            with tf.Session() as sess:
+                initial_a = tf.truncated_normal(shape=[config['batch'], step, config['dimension']],
+                                                mean=config['initial_mean'],
+                                                stddev=config['initial_std'])
+                a = tf.Variable(initial_a, name="action")
+                rnn_inst = config['optimizer'](a, step, config['batch'], config['domain'], config['instance'][index],
+                                               sess=sess)
+                the_best = rnn_inst.Optimize(config['epoch'], showbest=True)
+                records.append(the_best)
+        mean = np.mean(records)
+        std = np.std(records)
+        ci = 1.96*std/np.sqrt(10)
+        print "mean:{0}, std:{1}".format(mean, ci)
+
+for config in CONFIGURATIONS:
+    multiple_run(config, 10)
